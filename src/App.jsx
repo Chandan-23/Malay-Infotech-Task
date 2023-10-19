90bd1bf39c26e8a395462a2e37e3653dd1456175
Chandan Gowda H N <88780994+Chandan-23@users.noreply.github.com>
2023-10-19 17:14:15 +0530

    Add files via upload
    Please find the Attachments

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegisterForm from './Components/RegisterForm'
+import "./Components/Form.css"
+
+const App = () => {
+  return (
+    <div>
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default App
